efa44b127c0420397f581a5576647f8032760735
war1u5 <marius.wsf@gmail.com>
2023-10-29 21:29:46 +0200

    Added ASCII art file for welcome function

--- main.c
+++ main.c
@@ -1,9 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void open_and_display(const char *filename)
+{
+    FILE *f = fopen(filename, "r");
+    if (f == NULL)
+    {
+        printf("error opening file %s\n", filename);
+        exit(-1);
+    }
+
+    char c;
+    while ((c = fgetc(f)) != EOF)
+    {
+        printf("%c", c);
+    }
+
+    fclose(f);
+}
+
 void welcome()
 {
-    printf("Welcome to my app!\n");
+    open_and_display("./_ascii_art.txt");
+    printf("Test APP\n");
 }
 
 int main()
